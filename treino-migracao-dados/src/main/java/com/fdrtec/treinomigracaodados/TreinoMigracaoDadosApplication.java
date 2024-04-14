a86e2f8ef7c2ad0ea9584026df842ed3bb5b1c50
fdrtec <fdrtec@gmail.com>
2024-04-14 17:37:30 -0300

    feature/treino-migracao-dados: criacao de projeto

---
+++ treino-migracao-dados/src/main/java/com/fdrtec/treinomigracaodados/TreinoMigracaoDadosApplication.java
@@ -0,0 +1,13 @@
+package com.fdrtec.treinomigracaodados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoMigracaoDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoMigracaoDadosApplication.class, args);
+	}
+
+}
---
+++ treino-migracao-dados/src/test/java/com/fdrtec/treinomigracaodados/TreinoMigracaoDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdrtec.treinomigracaodados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoMigracaoDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
